706fb10b34e0a973552fb997f0a1f1dccef6c359
Sevan Quesnot <sevan.quesnot@gmail.com>
2026-06-03 14:58:01 +0200

    ADD | Redirect after login

--- controler/helpers/auth.php
+++ controler/helpers/auth.php
@@ -3,6 +3,7 @@
 function requireLogin() {
     if (session_status() !== PHP_SESSION_ACTIVE) session_start();
     if (!isset($_SESSION['email'])) {
+        $_SESSION['redirect'] = $_SERVER['REQUEST_URI'];
         header('Location: /app/login');
         exit();
     }
